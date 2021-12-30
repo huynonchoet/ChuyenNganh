428d51a3712b170e7698ebc8549c670d1e7f0f1f
nhutrong11355 <nguyennhutrong69@gmail.com>
2021-12-30 07:20:11 +0700

    mm

--- DuAn/app/Http/Controllers/admin/thongkecontroller.php
+++ DuAn/app/Http/Controllers/admin/thongkecontroller.php
@@ -7,34 +7,142 @@ use App\Http\Controllers\Controller;
 use App\order;
 use App\products;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class thongkecontroller extends Controller
 {
-    public function thongke(){
+    public function thongke()
+    {
+        $mathangbancham=$this->mathangbancham();
+        $mathangbanchay = $this->mathangbanchay();
+        $data_product_theocategory = $this->thongketheocategory();
+        $data_product_theobrand = $this->thongketheobrand();
+        $data_product_theothang = $this->thongketheothang();
+        return view("admin.thongke.thongke",compact('data_product_theothang', 'data_product_theocategory', 'data_product_theobrand', 'mathangbanchay', 'mathangbancham'));
+    }
+
+    public function thongketheothang()
+    {
+        $data_product_theothang = [];
         $data_detail_oder = detail_order::all();
         $data_order = order::all();
         $data_product = products::all();
-        $data_product_theothang=[];
-        for($i=1;$i<=12;$i++){
-            $total=0;
-            for($j=0;$j<count($data_order);$j++){
+        for ($i = 1; $i <= 12; $i++) {
+            $total = 0;
+            for ($j = 0; $j < count($data_order); $j++) {
                 $month = date('m', strtotime($data_order[$j]->day));
-                if($i==$month){
-                    for($h=0;$h<count($data_detail_oder);$h++){
-                        if($data_detail_oder[$h]->id_order==$data_order[$j]->id){
+                if ($i == $month) {
+                    for ($h = 0; $h < count($data_detail_oder); $h++) {
+                        if ($data_detail_oder[$h]->id_order == $data_order[$j]->id) {
 
-                            for($k=0;$k<count($data_product);$k++){
-                                if($data_product[$k]->id==$data_detail_oder[$h]->id_product){
-                                    $total=(int)$total+((int)$data_detail_oder[$h]->amount)*((int)$data_product[$k]->price);
+                            for ($k = 0; $k < count($data_product); $k++) {
+                                if ($data_product[$k]->id == $data_detail_oder[$h]->id_product) {
+                                    $total = (int)$total + ((int)$data_detail_oder[$h]->amount) * ((int)$data_product[$k]->price);
                                 }
                             }
                         }
                     }
                 }
             }
-            $data_product_theothang[$i]=$total;
+            $data_product_theothang[$i] = $total;
         }
-        // var_dump($data_product_theothang);
-        return view("admin.thongke.thongke",compact('data_product_theothang'));
+        return $data_product_theothang;
+    }
+
+    public function thongketheobrand()
+    {
+        $data_product_theobrand = [
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+        ];
+        $data_detail_oder = detail_order::all();
+        $data_product = products::all();
+        foreach ($data_product as $i) {
+            foreach ($data_detail_oder as $j) {
+                if ($i->id == $j->id_product) {
+                    if ($i->brand == 1) {
+                        $data_product_theobrand[1] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->brand == 2) {
+                        $data_product_theobrand[2] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->brand == 3) {
+                        $data_product_theobrand[3] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->brand == 4) {
+                        $data_product_theobrand[4] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->brand == 5) {
+                        $data_product_theobrand[5] += (int)$j->amount * (int)$i->price;
+                    }
+                }
+            }
+        }
+        return $data_product_theobrand;
+    }
+
+    public function thongketheocategory()
+    {
+        $data_product_theocategory = [
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+        ];
+        $data_detail_oder = detail_order::all();
+        $data_product = products::all();
+        foreach ($data_product as $i) {
+            foreach ($data_detail_oder as $j) {
+                if ($i->id == $j->id_product) {
+                    if ($i->category == 1) {
+                        $data_product_theocategory[1] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->category == 2) {
+                        $data_product_theocategory[2] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->category == 3) {
+                        $data_product_theocategory[3] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->category == 4) {
+                        $data_product_theocategory[4] += (int)$j->amount * (int)$i->price;
+                    }
+                    if ($i->category == 5) {
+                        $data_product_theocategory[5] += (int)$j->amount * (int)$i->price;
+                    }
+                }
+            }
+        }
+        return $data_product_theocategory;
+    }
+
+    public function mathangbanchay(){
+        $mathangbanchay = detail_order::select('id_product', detail_order::raw('SUM(amount) as total'))->groupBy('id_product')->orderBy('total', 'desc')->limit(3)->get();
+        $data_product = products::all();
+        foreach ($mathangbanchay as  $i) {
+            foreach ($data_product as $j) {
+                if($i->id_product==$j->id){
+                    $i->name=$j->name;
+                }
+            }
+        }
+        return $mathangbanchay;
+    }
+
+    public function mathangbancham()
+    {
+        $mathangbancham = detail_order::select('id_product', detail_order::raw('SUM(amount) as total'))->groupBy('id_product')->orderBy('total')->limit(3)->get();
+        $data_product = products::all();
+        foreach ($mathangbancham as  $i) {
+            foreach ($data_product as $j) {
+                if ($i->id_product == $j->id) {
+                    $i->name = $j->name;
+                }
+            }
+        }
+        return $mathangbancham;
     }
 }
--- DuAn/resources/views/admin/thongke/thongke.blade.php
+++ DuAn/resources/views/admin/thongke/thongke.blade.php
@@ -1,75 +1,192 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<div class="col-lg-6">
-          <div class="card">
+    <div class="col-lg-6">
+        <div class="card">
             <div class="card-body">
-              <h5 class="card-title">This Year's Revenue Statistics</h5>
+                <h2 class="card-title">3 Best Selling Item :</h2>
+                @foreach ($mathangbanchay as $item)
+                  <h5 class="card-title">{{ $item ->name}}</h5>
+                @endforeach
+            </div>
+        </div>
+    </div>
+        <div class="col-lg-6">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">3 Slowest Selling Item :</h2>
+                @foreach ($mathangbancham as $item)
+                  <h5 class="card-title">{{ $item ->name}}</h5>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">This Year's Revenue Statistics</h5>
 
-              <!-- Bar Chart -->
-              <canvas id="barChart" style="max-height: 400px;"></canvas>
-              <script>
-                var thongke = {!! json_encode($data_product_theothang, JSON_HEX_TAG) !!};
-                var thang1=thongke[1];
-                var thang2=thongke[2];
-                var thang3=thongke[3];
-                var thang4=thongke[4];
-                var thang5=thongke[5];
-                var thang6=thongke[6];
-                var thang7=thongke[7];
-                var thang8=thongke[8];
-                var thang9=thongke[9];
-                var thang10=thongke[10];
-                var thang11=thongke[11];
-                var thang12=thongke[12];
-                document.addEventListener("DOMContentLoaded", () => {
-                  new Chart(document.querySelector('#barChart'), {
-                    type: 'bar',
-                    data: {
-                      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July' , 'Augus' , 'September' , 'October' , 'November' , 'December'],
-                      datasets: [{
-                        label: 'Bar Chart',
-                        data: [thang1,thang2,thang3,thang4,thang5,thang6,thang7,thang8,thang9,thang10,thang11,thang12],
-                        backgroundColor: [
-                          'rgba(255, 99, 132, 0.2)',
-                          'rgba(255, 159, 64, 0.2)',
-                          'rgba(255, 205, 86, 0.2)',
-                          'rgba(75, 192, 192, 0.2)',
-                          'rgba(54, 162, 235, 0.2)',
-                          'rgba(153, 102, 255, 0.2)',
-                          'rgba(201, 203, 207, 0.2)',
-                          'rgba(174, 176, 179, 0.2',
-                          'rgb(162, 204, 54,0.2)',
-                          'rgb(77, 19, 45,0.2)'
-                        ],
-                        borderColor: [
-                          'rgb(255, 99, 132)',
-                          'rgb(255, 159, 64)',
-                          'rgb(255, 205, 86)',
-                          'rgb(75, 192, 192)',
-                          'rgb(54, 162, 235)',
-                          'rgb(153, 102, 255)',
-                          'rgb(201, 203, 207)',
-                          'rgba(174, 176, 179',
-                          'rgb(162, 204, 54)',
-                          'rgb(77, 19, 45)',
-                        ],
-                        borderWidth: 1
-                      }]
-                    },
-                    options: {
-                      scales: {
-                        y: {
-                          beginAtZero: true
-                        }
-                      }
-                    }
-                  });
-                });
-              </script>
-              <!-- End Bar CHart -->
+                <!-- Bar Chart -->
+                <canvas id="barChart" style="max-height: 400px;"></canvas>
+                <script>
+                    var thongke_theothang = {!! json_encode($data_product_theothang, JSON_HEX_TAG) !!};
+                    var thang1 = thongke_theothang[1];
+                    var thang2 = thongke_theothang[2];
+                    var thang3 = thongke_theothang[3];
+                    var thang4 = thongke_theothang[4];
+                    var thang5 = thongke_theothang[5];
+                    var thang6 = thongke_theothang[6];
+                    var thang7 = thongke_theothang[7];
+                    var thang8 = thongke_theothang[8];
+                    var thang9 = thongke_theothang[9];
+                    var thang10 = thongke_theothang[10];
+                    var thang11 = thongke_theothang[11];
+                    var thang12 = thongke_theothang[12];
+                    document.addEventListener("DOMContentLoaded", () => {
+                        new Chart(document.querySelector('#barChart'), {
+                            type: 'bar',
+                            data: {
+                                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augus',
+                                    'September', 'October', 'November', 'December'
+                                ],
+                                datasets: [{
+                                    label: 'Bar Chart',
+                                    data: [thang1, thang2, thang3, thang4, thang5, thang6, thang7, thang8,
+                                        thang9, thang10, thang11, thang12
+                                    ],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 205, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(201, 203, 207, 0.2)',
+                                        'rgba(174, 176, 179, 0.2',
+                                        'rgb(162, 204, 54,0.2)',
+                                        'rgb(77, 19, 45,0.2)'
+                                    ],
+                                    borderColor: [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(255, 159, 64)',
+                                        'rgb(255, 205, 86)',
+                                        'rgb(75, 192, 192)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(153, 102, 255)',
+                                        'rgb(201, 203, 207)',
+                                        'rgba(174, 176, 179',
+                                        'rgb(162, 204, 54)',
+                                        'rgb(77, 19, 45)',
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            }
+                        });
+                    });
+                </script>
+                <!-- End Bar CHart -->
 
             </div>
-          </div>
         </div>
+    </div>
+
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Statistics By Brand</h5>
+
+                <!-- Pie Chart -->
+                <canvas id="pieChart" style="max-height: 400px;"></canvas>
+                <script>
+                    var thongke_theobrand = {!! json_encode($data_product_theobrand, JSON_HEX_TAG) !!};
+                    var brand1 = thongke_theobrand[1];
+                    var brand2 = thongke_theobrand[2];
+                    var brand3 = thongke_theobrand[3];
+                    var brand4 = thongke_theobrand[4];
+                    var brand5 = thongke_theobrand[5];
+                    document.addEventListener("DOMContentLoaded", () => {
+                        new Chart(document.querySelector('#pieChart'), {
+                            type: 'pie',
+                            data: {
+                                labels: [
+                                    'LV',
+                                    'Channel',
+                                    'Gucci',
+                                    'Nike',
+                                    'Puma',
+                                ],
+                                datasets: [{
+                                    label: 'My First Dataset',
+                                    data: [brand1, brand2, brand3, brand4, brand5],
+                                    backgroundColor: [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(126, 55, 235)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(98, 162, 99)',
+                                        'rgb(11, 162, 27)'
+                                    ],
+                                    hoverOffset: 3
+                                }]
+                            }
+                        });
+                    });
+                </script>
+                <!-- End Pie CHart -->
+
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Statistics By Category</h5>
+
+                <!-- Doughnut Chart -->
+                <canvas id="doughnutChart" style="max-height: 400px;"></canvas>
+                <script>
+                    var thongke_theocategory = {!! json_encode($data_product_theocategory, JSON_HEX_TAG) !!};
+                    var category1 = thongke_theocategory[1];
+                    var category2 = thongke_theocategory[2];
+                    var category3 = thongke_theocategory[3];
+                    var category4 = thongke_theocategory[4];
+                    var category5 = thongke_theocategory[5];
+                    document.addEventListener("DOMContentLoaded", () => {
+                        new Chart(document.querySelector('#doughnutChart'), {
+                            type: 'doughnut',
+                            data: {
+                                labels: [
+                                    'Áo Nam',
+                                    'Áo Nữ',
+                                    'Quần Nam',
+                                    'Quần Nữ',
+                                    'Đồ bộ',
+                                ],
+                                datasets: [{
+                                    label: 'My First Dataset',
+                                    data: [category1, category2, category3,category4,category5],
+                                    backgroundColor: [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(126, 55, 235)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(98, 162, 99)',
+                                        'rgb(11, 162, 27)'
+                                    ],
+                                    hoverOffset: 4
+                                }]
+                            }
+                        });
+                    });
+                </script>
+                <!-- End Doughnut CHart -->
+
+            </div>
+        </div>
+    </div>
 @endsection
